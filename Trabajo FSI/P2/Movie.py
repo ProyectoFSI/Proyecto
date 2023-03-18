0e7d684c15c25c05ff74098881174188690e2289
plopcan <127615799+plopcan@users.noreply.github.com>
2023-03-18 16:13:35 +0100

    14/03/23

--- Trabajo FSI/P2/Movie.py
+++ Trabajo FSI/P2/Movie.py
@@ -14,3 +14,54 @@ class Movie (Show):
     def __eq__(self, other):
         return super().__eq__(other)
 
+    @property
+    def show_id(self):
+        return self.__show_id
+
+    @property
+    def tipo(self):
+        return self.__tipo
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def director(self):
+        return self.__director
+
+    @property
+    def cast(self):
+        return self.__cast
+
+    @property
+    def country(self):
+        return self.__country
+
+    @property
+    def date_added(self):
+        return self.__date_added
+
+    @property
+    def release_year(self):
+        return self.__release_year
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    @property
+    def listed_in(self):
+        return self.__listed_in
+
+    @property
+    def description(self):
+        return self.__description
+
+
+
+
--- Trabajo FSI/P2/Show.py
+++ Trabajo FSI/P2/Show.py
@@ -28,47 +28,77 @@ class Show:
     @property
     def show_id (self):
         return self.__show_id
-    @property
-    def tipo (self):
-        return self.__tipo
+    @show_id.setter
+    def show_id(self, show_id):
+        self.__show_id = show_id
     @property
     def title (self):
         return self.__title
+    @title.setter
+    def title(self, title):
+        self.__title = title
     @property
     def director (self):
         return self.__director
+    @director.setter
+    def director(self, director):
+        self.__director = director
     @property
     def cast (self):
         return self.__cast
+    @cast.setter
+    def cast(self, cast):
+        self.__cast = cast
     @property
     def country (self):
         return self.__country
+    @country.setter
+    def cauntry(self, country):
+        self.__country = country
     @property
     def date_added (self):
         return self.__date_added
+    @date_added.setter
+    def date_added(self, date_added):
+        self.__date_added = date_added
     @property
     def release_year (self):
         return self.__release_year
+    @release_year.setter
+    def release_year(self, release_year):
+        self.__releaser_year = release_year
     @property
     def rating (self):
         return self.__rating
+    @rating.setter
+    def rating(self, rating):
+        self.__rating = rating
     @property
     def duration (self):
         return self.__duration
+    @duration.setter
+    def duration(self, duration):
+        self.__duration = duration
     @property
     def listed_in (self):
         return self.__listed_in
+    @listed_in.setter
+    def listed_in(self, listed_in):
+        self.__listed_in = listed_in
     @property
     def description (self):
         return self.__description
+    @description.setter
+    def decription(self, description):
+        self.__description = description
     # toString
 
-    # Equals
 
     def __str__(self):
         return  "ID: " + self._show_id + "\t\nTitle " + self._title + "\t\n" +  self._director + "\n\t" + self._cast + "\t\n" + self._country + "\n\t" + self._date_added + \
             "\n\t" + self._release_year + "\t\n" + self._rating + "\n\t" + self._duration + "\n\t" + self._listed_in + "\n\t" + self._description
 
+    # Equals
     def __eq__(self, other):
         if isinstance(other, Show):
             return self._show_id == other._show_id
